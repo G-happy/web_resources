e89971fbcd335d604e73ededa8da4f416fc2997b
GHL <1569215575@qq.com>
2022-08-14 16:14:00 +0800

    理解项目,但没完全理解

--- src/icons/index.js
+++ src/icons/index.js
@@ -6,7 +6,7 @@ Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
 // console.log(req.keys()) // svg 路径数组
-console.dir(req)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
+// console.dir(req('./dashboard.svg'))
+const requireAll = requireContext => requireContext.keys().map(ele => requireContext(ele))
 // 调用 requireAll 方法, 并传参
 requireAll(req)
